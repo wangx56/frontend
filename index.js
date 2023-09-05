f2f267c162179a5e2b9f5b46a9dc404262caf710
wangx56 <125093567+wangx56@users.noreply.github.com>
2023-09-05 23:12:45 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-import { useReducer } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './index.css';
-import { fetchAPI, submitAPI } from '../../../utils/fakeAPI';
-import pages from '../../../utils/pages';
-import BookingForm from './BookingForm';
+import Hero from './Hero';
+import WeekSpecials from './WeekSpecials';
+import Testimonials from './Testimonials';
+import OurStory from './OurStory';
 
-const updateTimes = (availableTimes, date) => {
-  const response = fetchAPI(new Date(date));
-  return (response.length !== 0) ? response : availableTimes; 
-};
-
-const initializeTimes = initialAvailableTimes => 
-  [...initialAvailableTimes, ...fetchAPI(new Date())];
-
-const Bookings = () => {
-  const [
-    availableTimes, 
-    dispatchOnDateChange
-  ] = useReducer(updateTimes, [], initializeTimes);
-  const navigate = useNavigate();
-
-  const submitData = formData => {
-    const response = submitAPI(formData);
-    if (response) navigate(pages.get('confirmedBooking').path);
-  }; 
-
+const Home = () => {
   return (
-    <div className="bookings">
-      <h2>Table reservation</h2>
-      <BookingForm 
-        availableTimes={availableTimes} 
-        dispatchOnDateChange={dispatchOnDateChange} 
-        submitData={submitData} 
-      />
-    </div>
+    <>
+      <Hero />
+      <WeekSpecials />
+      <Testimonials />
+      <OurStory />
+    </>
   );
 };
 
-export default Bookings;
+export default Home;
